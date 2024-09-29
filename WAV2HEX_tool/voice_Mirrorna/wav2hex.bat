f8e1e6944bb371df2ee2dd540c9c10487f0ed0e2
todopapa <th2y-mehr@asahi-net.or.jp>
2024-09-29 17:53:52 +0900

    WAV2HEX tool

---
+++ WAV2HEX_tool/voice_Mirrorna/wav2hex.bat
@@ -0,0 +1,2 @@
+..\tool_voice\wav2hex\Debug\wav2hex.exe W25Q16.hex vos_mac.txt wavlist.txt 8000 8
+pause
---
+++ WAV2HEX_tool/voice_SANTA/wav2hex.bat
@@ -0,0 +1,2 @@
+..\tool_voice\wav2hex\Debug\wav2hex.exe W25Q64_santa.hex vos_mac.txt wavlist.txt 16000 8
+pause
